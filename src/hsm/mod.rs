c45fec8fa479618641c56c9527c8b182dfa0221c
Alexandre Escoubas <alexandre.escoubas@cscs.ch>
2025-05-09 16:33:05 +0200

    chore: update dependencies to match workspace

--- src/hsm/mod.rs
+++ src/hsm/mod.rs
@@ -1727,11 +1727,10 @@ pub mod hw_inventory {
             use serde_json::Value;
             use std::str::FromStr;
             use std::string::ToString;
-            use strum_macros::{AsRefStr, Display, EnumIter, EnumString, IntoStaticStr};
+            use strum_macros::{AsRefStr, Display, EnumString, IntoStaticStr};
 
             #[derive(
                 Debug,
-                EnumIter,
                 EnumString,
                 IntoStaticStr,
                 AsRefStr,
