75536460d4dff7b3345d2154f7352cc5e2340f7b
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-12-01 22:19:31 +0100

    fix: From trait to convert CFS.Component

--- src/cfs/component/http_client/v2/types.rs
+++ src/cfs/component/http_client/v2/types.rs
@@ -1,6 +1,5 @@
 use manta_backend_dispatcher::types::cfs::component::{
-  Component as FrontEndComponent,
-  State as FrontEndState,
+  Component as FrontEndComponent, State as FrontEndState,
 };
 
 use std::collections::HashMap;
@@ -116,20 +115,26 @@ pub struct Component {
 
 impl From<FrontEndComponent> for Component {
   fn from(component: FrontEndComponent) -> Self {
-    let mut state_vec = Vec::new();
-    for state in component.state.unwrap() {
-      let state = State {
-        clone_url: state.clone_url,
-        playbook: state.playbook,
-        commit: state.commit,
-        session_name: state.session_name,
-      };
-      state_vec.push(state);
-    }
+    let state_vec_opt = if let Some(component_state_vec) = component.state {
+      let mut state_vec: Vec<State> = Vec::new();
+      for state in component_state_vec {
+        let state = State {
+          clone_url: state.clone_url,
+          playbook: state.playbook,
+          commit: state.commit,
+          session_name: state.session_name,
+        };
+        state_vec.push(state);
+      }
+
+      Some(state_vec)
+    } else {
+      None
+    };
 
     Component {
       id: component.id,
-      state: Some(state_vec),
+      state: state_vec_opt,
       state_append: None,
       desired_config: component.desired_config,
       error_count: component.error_count,
