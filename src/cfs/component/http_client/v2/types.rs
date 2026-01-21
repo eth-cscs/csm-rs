9f9fe940a4e5db87bb7179231bfe640ebede5671
Manuel Sopena Ballesteros <masber@hotmail.com>
2026-01-21 10:08:24 +0100

    refactor: clean code

--- src/cfs/component/http_client/v2/types.rs
+++ src/cfs/component/http_client/v2/types.rs
@@ -6,50 +6,6 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
-/* #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct StateResponse {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "cloneUrl")]
-    pub clone_url: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub playbook: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub commit: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "sesisonName")]
-    pub session_name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "lastUpdated")]
-    pub last_updated: Option<String>,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ComponentResponse {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub id: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub state: Option<Vec<StateResponse>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "stateAppend")]
-    pub state_append: Option<StateResponse>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "desiredConfig")]
-    pub desired_config: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "errorCount")]
-    pub error_count: Option<u64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "retryPolicy")]
-    pub retry_policy: Option<u64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub enabled: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "configurationStatus")]
-    pub configuration_status: Option<String>, //values unconfigured, pending, failed, configured
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub tags: Option<HashMap<String, String>>,
-} */
-
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct State {
   #[serde(skip_serializing_if = "Option::is_none")]
