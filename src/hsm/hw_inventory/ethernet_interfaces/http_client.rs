1d4278c11be220cbfd883b6496a6958051779b06
Manuel Sopena Ballesteros <masber@hotmail.com>
2026-02-23 16:20:30 +0100

    feat: add logic to add a network interface

--- src/hsm/hw_inventory/ethernet_interfaces/http_client.rs
+++ src/hsm/hw_inventory/ethernet_interfaces/http_client.rs
@@ -1,6 +1,128 @@
-use crate::error::Error;
+use crate::{
+  error::Error,
+  hsm::hw_inventory::ethernet_interfaces::types::EthernetInterface,
+};
 
 use super::types::{ComponentEthernetInterface, IpAddressMapping};
+
+pub async fn post(
+  auth_token: &str,
+  base_url: &str,
+  root_cert: &[u8],
+  eht_interface: ComponentEthernetInterface,
+) -> Result<(), Error> {
+  let client_builder = reqwest::Client::builder()
+    .add_root_certificate(reqwest::Certificate::from_pem(root_cert)?)
+    .use_rustls_tls();
+
+  // Build client
+  let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
+    // socks5 proxy
+    log::debug!("SOCKS5 enabled");
+    let socks5proxy = reqwest::Proxy::all(socks5_env)?;
+
+    // rest client to authenticate
+    client_builder.proxy(socks5proxy).build()?
+  } else {
+    client_builder.build()?
+  };
+
+  // let api_url: String =
+  //   format!("{}/{}", base_url, "/hsm/v2/Inventory/EthernetInterfaces");
+  let api_url: String =
+    format!("{}/hsm/v2/Inventory/EthernetInterfaces", base_url);
+
+  let response = client
+    .post(api_url)
+    .bearer_auth(auth_token)
+    .json(&eht_interface)
+    .send()
+    .await?;
+
+  if let Err(e) = response.error_for_status_ref() {
+    match response.status() {
+      reqwest::StatusCode::UNAUTHORIZED => {
+        let error_payload = response.text().await?;
+        let error = Error::RequestError {
+          response: e,
+          payload: error_payload,
+        };
+        return Err(error);
+      }
+      _ => {
+        let error_payload = response.text().await?;
+        dbg!(&error_payload);
+        let error = Error::Message(error_payload);
+        return Err(error);
+      }
+    }
+  }
+
+  response
+    .json()
+    .await
+    .map_err(|error| Error::NetError(error))
+}
+
+pub async fn post_ip_addresses(
+  auth_token: &str,
+  base_url: &str,
+  root_cert: &[u8],
+  eht_interface: ComponentEthernetInterface,
+) -> Result<EthernetInterface, Error> {
+  let client_builder = reqwest::Client::builder()
+    .add_root_certificate(reqwest::Certificate::from_pem(root_cert)?)
+    .use_rustls_tls();
+
+  // Build client
+  let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
+    // socks5 proxy
+    log::debug!("SOCKS5 enabled");
+    let socks5proxy = reqwest::Proxy::all(socks5_env)?;
+
+    // rest client to authenticate
+    client_builder.proxy(socks5proxy).build()?
+  } else {
+    client_builder.build()?
+  };
+
+  let api_url: String = format!(
+    "{}/{}/{}/IPAddresses",
+    base_url,
+    "hsm/v2/Inventory/EthernetInterfaces",
+    eht_interface.component_id.as_ref().unwrap()
+  );
+
+  let response = client
+    .post(api_url)
+    .bearer_auth(auth_token)
+    .json(&eht_interface)
+    .send()
+    .await?;
+
+  if let Err(e) = response.error_for_status_ref() {
+    match response.status() {
+      reqwest::StatusCode::UNAUTHORIZED => {
+        let error_payload = response.text().await?;
+        let error = Error::RequestError {
+          response: e,
+          payload: error_payload,
+        };
+        return Err(error);
+      }
+      _ => {
+        let error_payload = response.text().await?;
+        let error = Error::Message(error_payload);
+        return Err(error);
+      }
+    }
+  }
+
+  response
+    .json()
+    .await
+    .map_err(|error| Error::NetError(error))
+}
 
 // Get list of network interfaces
 // ref --> https://csm12-apidocs.svc.cscs.ch/iaas/hardware-state-manager/operation/doCompEthInterfacesGetV2/
